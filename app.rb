9ad76d6065db2232f1af56e5f8379021b01f1001
jason ho <43012595+JasonHo404@users.noreply.github.com>
2023-09-17 16:49:39 +0000

    92.47 percent

--- app.rb
+++ app.rb
@@ -10,14 +10,13 @@ get("/") do
 end
 
 get("/square/new") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  
   erb(:square)
 end
 
 get("/square/results") do
+  @x = params.fetch("user_number").to_f
+  @y = @x ** 2
 
 
   
@@ -25,25 +24,46 @@ get("/square/results") do
 end
 
 get("/square_root/new") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-  erb(:hi)
+
+  erb(:square_roots)
+end
+
+get("/square_root/results") do
+  @x = params.fetch("user_number").to_f
+  @y = Math.sqrt(@x)
+  erb(:square_roots_results)
 end
 
 get("/payment/new") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-  erb(:hi)
+
+  erb(:payment)
 end
 
+
 get("/random/new") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-  erb(:hi)
+
+  erb(:random)
+end
+
+get("/random/results") do
+  @x = params.fetch("user_min").to_f
+  @y = params.fetch("user_max").to_f
+  @z = rand(@x..@y)
+  erb(:random_results)
+end
+
+
+
+get("/payment/results") do
+  r = (params.fetch("apr").to_f / 100 )/ 12
+  n = params.fetch("years").to_f * 12
+  d1 = (1 + r)
+  d2 = (n *-1)
+ 
+  denominator = 1 - (d1 ** d2)
+  @principal = params.fetch("principal").to_f
+  @payment = (r * @principal)/denominator
+  @payment= @payment.to_fs(:currency)
+
+  erb(:payment_results)
 end
