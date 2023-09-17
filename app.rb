8aa19c4b13ad90b718628817c5fe96b44b7463c9
jason ho <43012595+JasonHo404@users.noreply.github.com>
2023-09-17 14:40:33 +0000

    v1

--- app.rb
+++ app.rb
@@ -6,4 +6,44 @@ get("/") do
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  erb(:hi)
+end
+
+get("/square/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+  erb(:square)
+end
+
+get("/square/results") do
+
+
+  
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+  erb(:hi)
+end
+
+get("/payment/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+  erb(:hi)
+end
+
+get("/random/new") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+  erb(:hi)
 end
